d6a9b5b5907d3b14956e52930b22e1800168b2cb
anon <anon@anon.com>
2023-09-17 06:35:55 -0600

    script

--- scripts/init-lab.sh
+++ scripts/init-lab.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
+MINIKUBE_MEMORY=8192
+MINIKUBE_CPUS=2
+
 rm -rf ~/.kube/config
 export GIT_ROOT=$(git rev-parse --show-toplevel)
 export KUBECTL_CLI="$GIT_ROOT/scripts/kubectl"
 export VCLUSTER_CLI="$GIT_ROOT/scripts/vcluster"
 
-$GIT_ROOT/scripts/get-cli-tools.sh
-
 export NAME="lab-hub"
-export KUBECONFIG="$GIT_ROOT/tests/kubeconfig.$NAME.kubeconfig"
+export KUBECONFIG="$GIT_ROOT/scripts/kubeconfig.$NAME.kubeconfig.tmp"
+
+$GIT_ROOT/scripts/get-cli-tools.sh
 
 if [[ "$1" == "-f" ]] || [[ ! -f "$KUBECONFIG" ]]; then
 rm -rf $KUBECONFIG
@@ -16,10 +19,9 @@ minikube delete -p "$NAME"
 fi
 
 # start the cluster
-minikube start -p "$NAME" --network bridge
+minikube start -p "$NAME" --memory $MINIKUBE_MEMORY --cpus $MINIKUBE_CPUS --network bridge
 # init the Hub cluster
 KUBECONFIG="$KUBECONFIG" $GIT_ROOT/scripts/init-hub.sh
-
 
 cp $KUBECONFIG ~/.kube/config
 echo "FILE AT: $KUBECONFIG"
