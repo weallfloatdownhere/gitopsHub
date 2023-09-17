4327056694f7882861466ec05aa82cd4bd0bd829
anon <anon@anon.com>
2023-09-16 22:59:34 -0600

    arg

---
+++ init-hub-cluster.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+GIT_ROOT=$(git rev-parse --show-toplevel)
+
+if [[ -z "$KUBECONFIG" ]]; then
+echo "USAGE: KUBECONFIG=/home/user/kubeconfig ./init-hub-cluster.sh"
+exit 1
+elif [[ ! -f "$KUBECONFIG" ]]; then
+echo "ERROR - Specified kubeconfig file not found."
+exit 1
+fi
+
+kubectl apply -k "$GIT_ROOT/administration/resources/argo-cd/base"
+sleep 1
+kubectl wait --for=condition=available deployment -l "app.kubernetes.io/name=argocd-server" -n argocd --timeout=300s
+kubectl apply -k "$GIT_ROOT/administration/resources/root.yaml"
