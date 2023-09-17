709908a8732125346f79a31a432514f21fb5a0ca
anon <anon@anon.com>
2023-09-17 06:21:17 -0600

    env

--- tests/init-lab.sh
+++ tests/init-lab.sh
@@ -5,7 +5,7 @@ export GIT_ROOT=$(git rev-parse --show-toplevel)
 export KUBECTL_CLI="$GIT_ROOT/scripts/kubectl"
 export VCLUSTER_CLI="$GIT_ROOT/scripts/vcluster"
 
-$GIT_ROOT/scripts/_get-cli-tools.sh
+$GIT_ROOT/scripts/get-cli-tools.sh
 
 export NAME="lab-hub"
 export KUBECONFIG="$GIT_ROOT/tests/kubeconfig.$NAME.kubeconfig"
@@ -17,9 +17,9 @@ fi
 
 # start the cluster
 minikube start -p "$NAME" --network bridge
-
 # init the Hub cluster
 KUBECONFIG="$KUBECONFIG" $GIT_ROOT/scripts/init-hub.sh
+
 
 cp $KUBECONFIG ~/.kube/config
 echo "FILE AT: $KUBECONFIG"
