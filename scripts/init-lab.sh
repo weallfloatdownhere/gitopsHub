65378582958f9607616215ce469cac91f4cb7615
anon <anon@anon.com>
2023-09-18 13:50:47 -0600

    dev

--- scripts/init-lab.sh
+++ scripts/init-lab.sh
@@ -2,19 +2,15 @@
 
 # INITILIZATION STEP
 export GIT_ROOT=$(git rev-parse --show-toplevel)
-export KUBECTL_CLI="$GIT_ROOT/scripts/kubectl"
-export VCLUSTER_CLI="$GIT_ROOT/scripts/vcluster"
 
 export HUB_MINIKUBE_MEMORY=3078
 export HUB_MINIKUBE_CPUS=2
-export HUB_OUTPUT_DIR="$GIT_ROOT/administration/hubs"
 
 export SPOKE_MINIKUBE_MEMORY=3078
 export SPOKE_MINIKUBE_CPUS=2
-export SPOKE_OUTPUT_DIR="$GIT_ROOT/environments/laboratory/spokes"
 
-if [[ -z "$1" ]]; then
 echo "USAGE: ./init-lab.sh {hub|spoke|all}"
+if [[ -z "$1" ]]; then
 exit 1
 fi
 
@@ -23,23 +19,27 @@ $GIT_ROOT/scripts/get-cli-tools.sh
 
 # START HUB LAB STEP
 if [[ "$1" == "hub" ]] || [[ "$1" == "all" ]]; then
-export NAME="hub-lab-1"
-export OUTDIR="$HUB_OUTPUT_DIR/$NAME"
+export NAME="lab-1"
+export OUTDIR="$GIT_ROOT/administration/hubs/$NAME"
 rm -rf "$OUTDIR" && mkdir -p "$OUTDIR"
 export KUBECONFIG="$OUTDIR/kubeconfig.tmp"
 minikube delete -p "$NAME"
 minikube start -p "$NAME" --memory $HUB_MINIKUBE_MEMORY --cpus $HUB_MINIKUBE_CPUS --network bridge
+kubectl config view --context "$NAME" --minify --flatten > $KUBECONFIG.tmp
+rm -rf $KUBECONFIG && mv $KUBECONFIG.tmp $KUBECONFIG
 KUBECONFIG="$KUBECONFIG" $GIT_ROOT/scripts/init-hub.sh
 fi
 
 # START SPOKE LAB STEP
 if [[ "$1" == "spoke" ]] || [[ "$1" == "all" ]]; then
-export NAME="spoke-lab-1"
-export OUTDIR="$SPOKE_OUTPUT_DIR/$NAME"
+export NAME="lab-1"
+export OUTDIR="$GIT_ROOT/environments/laboratory/spokes/$NAME"
 rm -rf "$OUTDIR" && mkdir -p "$OUTDIR"
-export KUBECONFIG="$OUTDIR/kubeconfig.tmp"
+kubectl config view --context "$NAME" --minify --flatten > $KUBECONFIG.tmp
+rm -rf $KUBECONFIG && mv $KUBECONFIG.tmp $KUBECONFIG
 minikube delete -p "$NAME"
 minikube start -p "$NAME" --memory $SPOKE_MINIKUBE_MEMORY --cpus $SPOKE_MINIKUBE_CPUS --network bridge
+kubectl config view --minify --flatten > $KUBECONFIG
 fi
 
 #cp $KUBECONFIG ~/.kube/config
