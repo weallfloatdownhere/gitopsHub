e99baffd3c1ca5ea23a83a3139b8d5af01ff3ea4
anon <anon@anon.com>
2023-09-18 12:56:21 -0600

    hubs

--- scripts/init-lab.sh
+++ scripts/init-lab.sh
@@ -1,27 +1,46 @@
 #!/usr/bin/env bash
 
-MINIKUBE_MEMORY=7897
-MINIKUBE_CPUS=2
-
-rm -rf ~/.kube/config
+# INITILIZATION STEP
 export GIT_ROOT=$(git rev-parse --show-toplevel)
 export KUBECTL_CLI="$GIT_ROOT/scripts/kubectl"
 export VCLUSTER_CLI="$GIT_ROOT/scripts/vcluster"
 
-export NAME="lab-hub"
-export KUBECONFIG="$GIT_ROOT/scripts/kubeconfig.$NAME.kubeconfig.tmp"
+export HUB_MINIKUBE_MEMORY=3078
+export HUB_MINIKUBE_CPUS=2
+export HUB_OUTPUT_DIR="$GIT_ROOT/administration/hubs"
 
-$GIT_ROOT/scripts/get-cli-tools.sh
+export SPOKE_MINIKUBE_MEMORY=3078
+export SPOKE_MINIKUBE_CPUS=2
+export SPOKE_OUTPUT_DIR="$GIT_ROOT/environments/laboratory/spokes"
 
-if [[ "$1" == "-f" ]] || [[ ! -f "$KUBECONFIG" ]]; then
-rm -rf $KUBECONFIG
-minikube delete -p "$NAME"
+if [[ -z "$1" ]]; then
+echo "USAGE: ./init-lab.sh {hub|spoke|all}"
+exit 1
 fi
 
-# start the cluster
-minikube start -p "$NAME" --memory $MINIKUBE_MEMORY --cpus $MINIKUBE_CPUS --network bridge
-# init the Hub cluster
-KUBECONFIG="$KUBECONFIG" $GIT_ROOT/scripts/init-hub.sh
+rm -rf ~/.kube/config
+$GIT_ROOT/scripts/get-cli-tools.sh
 
-cp $KUBECONFIG ~/.kube/config
-echo "FILE AT: $KUBECONFIG"
+# START HUB LAB STEP
+if [[ "$1" == "hub" ]] || [[ "$1" == "all" ]]; then
+export NAME="hub-lab-1"
+export OUTDIR="$HUB_OUTPUT_DIR/$NAME"
+rm -rf "$OUTDIR" && mkdir -p "$OUTDIR"
+export KUBECONFIG="$OUTDIR/kubeconfig.tmp"
+minikube delete -p "$NAME"
+minikube start -p "$NAME" --memory $HUB_MINIKUBE_MEMORY --cpus $HUB_MINIKUBE_CPUS --network bridge
+KUBECONFIG="$KUBECONFIG" $GIT_ROOT/scripts/init-hub.sh
+fi
+
+# START SPOKE LAB STEP
+if [[ "$1" == "spoke" ]] || [[ "$1" == "all" ]]; then
+export NAME="spoke-lab-1"
+export OUTDIR="$SPOKE_OUTPUT_DIR/$NAME"
+rm -rf "$OUTDIR" && mkdir -p "$OUTDIR"
+export KUBECONFIG="$OUTDIR/kubeconfig.tmp"
+minikube delete -p "$NAME"
+minikube start -p "$NAME" --memory $SPOKE_MINIKUBE_MEMORY --cpus $SPOKE_MINIKUBE_CPUS --network bridge
+fi
+
+#cp $KUBECONFIG ~/.kube/config
+#echo "FILE AT: $KUBECONFIG"
