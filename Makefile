abea83a8349d462f14e4a8f31bb633ccac7012b3
anon <anon@anon.com>
2023-09-27 06:53:14 -0600

    argocd

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
+KUBECONFIG=${PWD}/manager/manager.kubeconfig
+
 install:
-	kubectl create ns argocd
 	kubectl apply -n argocd -k bootstrap/argocd/overlay
 	kubectl wait --for=condition=available deployment -l "app.kubernetes.io/name=argocd-server" -n argocd --timeout=300s
 	kubectl apply -n argocd -f bootstrap/bootstrap.yaml
--- local-env.sh
+++ local-env.sh
@@ -4,8 +4,8 @@ GIT_ROOT="$(git rev-parse --show-toplevel)"
 CONTEXT="manager"
 KUBECONFIG="$GIT_ROOT/manager/manager.kubeconfig"
 
+minikube delete --all
 rm -rf $KUBECONFIG
-minikube delete -p manager| true
 minikube start -p manager --memory 3078 --cpus 2 --network bridge
 
 kubectl config view --context $CONTEXT --flatten --minify > "$GIT_ROOT/k.tmp"
