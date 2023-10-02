181800bc46729ad92080c63219ff667588912580
anon <anon@anon.com>
2023-10-02 00:13:20 -0600

    hooks script

--- .hooks/run.sh
+++ .hooks/run.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
 
-# Repository hooks entrypoint.
+echo "${0}"
 
-GIT_ROOT=$(git rev-parse --show-toplevel)
-
-echo Find all .hooks/ directories in the repository and run every scripts in them.
-
-#for d in "$GIT_ROOT/manager/bootstrap/*" ; do
-#  for FILE in $d/*; do
-#    wave='"'$(basename $(dirname $FILE) | cut -d '-' -f3)'"' yq -i e '.metadata.annotations.["argocd.argoproj.io/sync-wave"] = env(wave)' $FILE;
-#  done
-#done
+for hookdir in "$(find $(git rev-parse --show-toplevel)/* -name ".hooks" -type d)" ; do
+  for hookscript in $hookdir/*; do
+    $hookscript;
+  done
+done
--- manager/bootstrap/.hooks/sync-waves.sh
+++ manager/bootstrap/.hooks/sync-waves.sh
@@ -1,8 +1,8 @@
 #/usr/bin/env bash
 
-GIT_ROOT=$(git rev-parse --show-toplevel)
+echo "${0}"
 
-for d in "$GIT_ROOT/manager/bootstrap/*" ; do
+for d in "$(git rev-parse --show-toplevel)/manager/bootstrap/*" ; do
   for FILE in $d/*; do
     wave='"'$(basename $(dirname $FILE) | cut -d '-' -f3)'"' yq -i e '.metadata.annotations.["argocd.argoproj.io/sync-wave"] = env(wave)' $FILE;
   done
