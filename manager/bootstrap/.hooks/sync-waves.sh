e939c767e6415a0ff68f2c7bdf4dc02affec84f8
anon <anon@anon.com>
2023-10-01 23:57:14 -0600

    finilizer

--- manager/bootstrap/.hooks/sync-waves.sh
+++ manager/bootstrap/.hooks/sync-waves.sh
@@ -1,12 +1,9 @@
 #/usr/bin/env bash
 
 GIT_ROOT=$(git rev-parse --show-toplevel)
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 for d in "$GIT_ROOT/manager/bootstrap/*" ; do
   for FILE in $d/*; do
-    YAML=$(basename $FILE);
-    WAVE=$(basename $(dirname $FILE) | cut -d '-' -f3);
-    yq -i e '.metadata.annotations.["argocd.argoproj.io/sync-wave"] = "env(WAVE)"' $YAML;
+    wave='"'$(basename $(dirname $FILE) | cut -d '-' -f3)'"' yq -i e '.metadata.annotations.["argocd.argoproj.io/sync-wave"] = env(wave)' $FILE;
   done
 done
